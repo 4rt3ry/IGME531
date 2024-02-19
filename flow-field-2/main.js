6e63fc47003c40b5939ade4a99370fc8c0e2646c
apowers131 <arthurpow131@gmail.com>
2024-02-19 11:16:08 -0500

    idk what I'm committing, but it looks like a decent flow field configuration

--- flow-field-2/main.js
+++ flow-field-2/main.js
@@ -1,7 +1,7 @@
 import { FlowField } from "./FlowField.js"
 import { FlowFieldCanvas } from "./FlowFieldCanvas.js"
 
-const field = new FlowField(1000, 1000, 10, 10, 100, 50);
-field.draw("original");
-// const field = new FlowFieldCanvas(2000, 2000, 10, 10, 100, 50);
-// field.draw("canvas", "transparent");
+// const field = new FlowField(500, 500, 10, 10, 100, 50);
+// field.draw("original");
+const field = new FlowFieldCanvas(2000, 2000, 10, 10, 100, 50);
+field.draw("canvas", "transparent");
