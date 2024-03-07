b02e994b5d611fc8e610d65b2ec49a63f22ef6f7
Arthur Powers (RIT Student) <aap5744@rit.edu>
2024-03-07 12:06:37 -0500

    made cool looking fractal plant

--- l-systems/FractalPlant.js
+++ l-systems/FractalPlant.js
@@ -11,8 +11,18 @@ class FractalPlant {
         this.angle = 25 * Math.PI / 180;
         this.data = 'X'
     }
-    draw(selector) {
-        const length = 5;
+    draw(selector, centerX, centerY, transformFunction) {
+        const angleDiff = 360 / 6;
+        const shapes = []
+        for (let i = 0; i < 360; i+= angleDiff) {
+            const t = (transformFunction ? transformFunction(i) : svg.transform().rotate(-25).translate(-100, 0).rotate(i)).translate(centerX, centerY).transform;
+            shapes.push(svg.group(this.getFractal(), t));
+        }
+        document.querySelector(`${selector}`).innerHTML = svg.svgWrapper(shapes.join(""), this.width, this.height, 0, 0, this.width, this.height);
+
+    }
+    getFractal() {
+        const length = 2;
         const shapes = [];
         const stack = [];
         let angle = this.angle;
@@ -32,12 +42,13 @@ class FractalPlant {
                     let y = p[1];
                     p[0] += Math.cos(angle) * length;
                     p[1] += Math.sin(angle) * length;
-                    shapes.push(svg.line(x, y, p[0], p[1]))
+                    shapes.push(svg.line(x, y, p[0], p[1], `rgb(${255 - stack.length * 20 - 20}, ${20}, ${100})`));
                     break;
             }
         }
 
-        document.querySelector(`${selector}`).innerHTML = svg.svgWrapper(shapes.join(""), this.width, this.height, 0, 0, this.width, this.height);
+        return shapes.join("");
+        document.querySelector(`${selector}`).innerHTML = svg.svgWrapper(shapes.join(""), this.width, this.height, xOffset, yOffset, this.width, this.height);
 
     }
     compile(iterations = 1) {
--- l-systems/main.js
+++ l-systems/main.js
@@ -1,5 +1,7 @@
 import { FractalPlant } from './FractalPlant.js'
+import * as svg from "./svg.js"
 
-const plant = new FractalPlant(800, 800);
-plant.compile(6);
-plant.draw("#original");
+const plant = new FractalPlant(1500, 1500);
+plant.compile(7);
+// plant.draw("#original", 750, 500, (angle) => svg.transform().translate(-500, 0).rotate(angle));
+plant.draw("#original", 750, 750, (angle) => svg.transform().rotate(-25).translate(-100, 0).rotate(angle));
