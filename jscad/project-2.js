0a4c1faaee81f2465d7b3de8df898b4abd7d833f
apowers131 <arthurpow131@gmail.com>
2024-04-09 00:33:39 -0400

    finished project 2

--- jscad/project-2.js
+++ jscad/project-2.js
@@ -1,7 +1,7 @@
 import { render } from './render.js';
 
 const { booleans, colors, primitives, transforms, geometries } = jscadModeling // modeling comes from the included MODELING library
-const { translate, scale } = transforms;
+const { translate, scale, rotate } = transforms;
 
 const { intersect, subtract, union } = booleans
 const { colorize, colorNameToRgb } = colors
@@ -9,23 +9,35 @@ const { cube, cuboid, sphere, polyhedron, geodesicSphere } = primitives
 const { toPoints } = geometries.geom3
 
 const d20 = (params) => {
-    return geodesicSphere({...params, frequency: 6})
+    return geodesicSphere({ ...params, frequency: 6 })
 }
 
 const fractal = (params, iterations) => {
-    const d20p = {radius: params.size}
+    const d20Params = { radius: params.size }
     if (iterations === 0) {
-        return d20({radius: params.size, frequency: 6});
+        return d20({ radius: params.size, frequency: 6 });
     }
-    console.log(toPoints(d20(d20p)))
-    const next = toPoints(d20(d20p)).map(point => {
+    const d20Points = uniq(toPoints(d20(d20Params)).flat());
+    const next = d20Points.map(point => {
         const mag = Math.sqrt(point[0] * point[0] + point[1] * point[1] + point[2] * point[2]);
         const p = point.map(i => i * 0.5);
-        debugger
-        return translate([p[0], p[1], p[2]], fractal({size: params.size / 2}, iterations - 1));
+        // debugger
+        return union(translate([p[0], p[1], p[2]], fractal({ size: params.size / 2 }, iterations - 1)));
     });
     return next;
 }
 
+/**
+ * 
+ * @param {*} a 
+ * @author https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
+ * @returns 
+ */
+function uniq(a) {
+    var seen = {};
+    return a.filter(function (item) {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
+}
 
-render(document.getElementById("render"), fractal({ size: 100 }, 1));
+render(document.getElementById("render"), rotate([Math.acos(-Math.sqrt(5)/3) / 2, 0, 0], fractal({ size: 100 }, 3)));
